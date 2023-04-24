602595dd66125a8e7f2270c32dcc70801df320e0
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-04-24 17:20:02 +0500

    Create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {Admin, LogIn, Seller, SignUp} from "./pages";
-import {Spinner} from "@chakra-ui/react";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Admin, LogIn, Seller, SignUp } from "./pages";
+import { Spinner } from "@chakra-ui/react";
 
 const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <Admin />
-        },
-        {
-            path: "/sign-up",
-            element: <SignUp />
-        },
-        {
-            path: "/login",
-            element: <LogIn />
-        },
-        {
-            path: "/seller",
-            element: <Seller />
-        }
-    ])
+  {
+    path: "/",
+    element: <Admin />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    path: "/login",
+    element: <LogIn />,
+  },
+  {
+    path: "/seller",
+    element: <Seller />,
+  },
+]);
 function App() {
   return (
-      <RouterProvider router={router}
-                      fallbackElement={<Spinner thickness='4px'
-                                                speed='0.65s'
-                                                emptyColor='gray.200'
-                                                color='blue.500'
-                                                size='xl' />}
-      />
-    )
+    <RouterProvider
+      router={router}
+      fallbackElement={
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      }
+    />
+  );
 }
 
 export default App;
--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,5 +1,117 @@
-function LogIn() {
-    return <>Log In</>;
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Card,
+  FormControl,
+  FormLabel,
+  Image,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
+
+function Login() {
+  const [showPasswordBtn, setShowPasswordBtn] = useState(false);
+  const [showPassword, setShowPassword] = useState(true);
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <Card
+        sx={{
+          width: "450px",
+          display: "flex",
+          alignItems: "center",
+          gap: "10px",
+          padding: "20px",
+        }}
+      >
+        <Image src="/src/assets/react.svg" alt="React logo" width="70px" />
+        <Text as="b" fontSize="2xl" color={"gray.700"}>
+          Yangi hisob yaratish
+        </Text>
+        <Text
+          sx={{
+            fontSize: "lg",
+            marginBottom: "5px",
+          }}
+        >
+          Agar hisobingiz bo&apos;lmasa{" "}
+          <Link
+            style={{
+              color: "#2B6CB0",
+            }}
+            to="/sign-up"
+          >
+            Hisobga yaratish
+          </Link>
+        </Text>
+        <FormControl
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            gap: "6px",
+          }}
+          isRequired
+        >
+          <FormLabel htmlFor="username">Foydalanuvchining nomi</FormLabel>
+          <InputGroup marginTop="-10px">
+            <Input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Foydalanuvchining nomi"
+            />
+            <InputRightElement>
+              <CheckIcon color="green.500" fontSize="sm" />
+            </InputRightElement>
+          </InputGroup>
+
+          <FormLabel htmlFor="password">Foydalanuvchining paroli</FormLabel>
+          <InputGroup marginTop="-10px">
+            <Input
+              type={showPassword ? "password" : "text"}
+              name="password"
+              id="password"
+              placeholder="Foydalanuvchining paroli"
+            />
+            <InputRightElement>
+              {showPasswordBtn && (
+                <Button
+                  h="1.75rem"
+                  size="sm"
+                  onMouseLeave={() => setShowPasswordBtn(false)}
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  show
+                </Button>
+              )}
+              {!showPasswordBtn && (
+                <CheckIcon
+                  color="green.500"
+                  fontSize="sm"
+                  onMouseMove={() => setShowPasswordBtn(true)}
+                />
+              )}
+            </InputRightElement>
+          </InputGroup>
+          <Button colorScheme='blue' sx={{
+            marginTop:"15px"
+          }}>Kirish</Button>
+        </FormControl>
+      </Card>
+    </Box>
+  );
 }
 
-export default LogIn;
+export default Login;
